a7b6ed65bbe65f303665d393cf56b5b0a5a066c0
Patronum129 <diego88rising@gmail.com>
2021-12-19 17:14:50 +0100

    .

--- Game/Source/Scene.cpp
+++ Game/Source/Scene.cpp
@@ -33,30 +33,79 @@ bool Scene::Awake()
 // Called before the first frame
 bool Scene::Start()
 {
-	// Load map
-	//app->map->Load("map1.tmx");
 	img = app->tex->Load("Assets/Textures/map1.png");
 	//app->audio->PlayMusic("Assets/Audio/Music/Retro_Platforming_David_Fesliyan.ogg");
 	app->render->camera.x = app->render->camera.y = 0;
+	world = new PhysicWorld({ 0,10 });
+
+	InitScene();
+
+	walls[0] = new PhysicBody({ 0,0 }, BodyType::STATIC, 1280, 5);
+	walls[1] = new PhysicBody({ 0,0 }, BodyType::STATIC, 5, 720);
+	walls[2] = new PhysicBody({ 1275,0 }, BodyType::STATIC, 5, 720);
+
+	floor[0] = new PhysicBody({ 0,576 }, BodyType::STATIC, 560, 144);
+	floor[1] = new PhysicBody({ 560,672 }, BodyType::STATIC, 192, 48);
+	floor[2] = new PhysicBody({ 752,576 }, BodyType::STATIC, 560, 144);
+	floor[3] = new PhysicBody({ 96,416 }, BodyType::STATIC, 32, 32);
+	floor[4] = new PhysicBody({ 384,544 }, BodyType::STATIC, 32, 32);
+	floor[5] = new PhysicBody({ 928,416 }, BodyType::STATIC, 32, 32);
+
+	for (int i = 0; i < 3; i++)
+	{
+		world->AddPhysicBody(walls[i]);
+	}
+
+	for (int i = 0; i < 3; i++)
+	{
+		world->AddPhysicBody(floor[i]);
+	}
 
 	return true;
+}
+
+void Scene::InitScene()
+{
+	// Obstacles
+	GameObject* g = new GameObject("wall", "Wall", _app);
+	// +8 = offset, porque pivot de b2Body es el centro, y de tectura es izquierda superior.
+	g->rBody = new PhysicBody({ 100,100 }, BodyType::STATIC, 16, 16);
+	world->AddPhysicBody(g->rBody);
+	gameObjects.add(g);
+
+	GameObject* j = new GameObject("floor", "floor", _app);
+	j->rBody = new PhysicBody({ 100,100 }, BodyType::STATIC, 16, 16);
+	world->AddPhysicBody(j->rBody);
+	gameObjects.add(j);
 }
 
 // Called each loop iteration
 bool Scene::PreUpdate()
 {
+	for (int i = 0; i < gameObjects.count(); i++)
+	{
+		gameObjects[i]->PreUpdate();
+		if (gameObjects[i]->pendingToDelete == true)
+		{
+			if (gameObjects[i]->rBody != nullptr)
+			{
+				world->DelPhysicBody(gameObjects[i]->rBody);
+			}
+			gameObjects[i]->CleanUp();
+			gameObjects.del(gameObjects.At(gameObjects.find(gameObjects[i])));
+		}
+	}
 	return true;
 }
 
 // Called each loop iteration
 bool Scene::Update(float dt)
 {
-	// Set the window title with map/tileset info
-	SString title("Map:%dx%d Tiles:%dx%d Tilesets:%d",
-		app->map->mapData.width, app->map->mapData.height,
-		app->map->mapData.tileWidth, app->map->mapData.tileHeight,
-		app->map->mapData.tilesets.count());
-
+	world->Update(1.0 / 60);
+	for (int i = 0; i < gameObjects.count(); i++)
+	{
+		gameObjects[i]->Update();
+	}
 	return true;
 }
 
@@ -66,6 +115,21 @@ bool Scene::PostUpdate()
 	bool ret = true;
 
 	app->render->DrawTexture(img, 0, 0);
+
+	for (int i = 0; i < gameObjects.count(); i++)
+	{
+		gameObjects[i]->PostUpdate();
+	}
+
+	app->render->DrawRectangle({ 0,0,1280,5 }, 255, 0, 0);
+	app->render->DrawRectangle({ 1275,0,5,720 }, 255, 0, 0);
+	app->render->DrawRectangle({ 0,0,5,720 }, 255, 0, 0);
+	app->render->DrawRectangle({ 0,576,560,144 }, 255, 0, 255, 70);
+	app->render->DrawRectangle({ 560,672,192,48 }, 255, 0, 255, 70);
+	app->render->DrawRectangle({ 752,576,560,144 }, 255, 0, 255, 70);
+	app->render->DrawRectangle({ 96,416,32,32 }, 255, 0, 255, 70);
+	app->render->DrawRectangle({ 384,544,32,32 }, 255, 0, 255, 70);
+	app->render->DrawRectangle({ 928,416,32,32 }, 255, 0, 255, 70);
 
 	if(app->input->GetKey(SDL_SCANCODE_ESCAPE) == KEY_DOWN)
 		ret = false;
@@ -78,48 +142,34 @@ bool Scene::CleanUp()
 {
 	LOG("Freeing scene");
 	RELEASE(world);
+	for (int i = 0; i < gameObjects.count(); i++)
+	{
+		if (gameObjects[i] != nullptr)
+		{
+			gameObjects[i]->CleanUp();
+		}
+	}
+
+	gameObjects.clear();
+
+	for (int i = 0; i < sceneTextures.count(); i++)
+	{
+		if (sceneTextures[i])
+		{
+			_app->tex->UnLoad(sceneTextures[i]);
+			sceneTextures[i] = nullptr;
+		}
+	}
 
 	return true;
 }
 
-void Scene::Activate()
+void Scene::DestroyGameObject(GameObject* gameObject)
 {
-	Module::Activate();
+	int index = gameObjects.find(gameObject);
 
-	//app->physics->Activate();
-	//app->playerModule->Activate();
-	app->map->Activate();
-	//app->enemies->Activate();
-
-	InitMapLevel();
-}
-
-void Scene::Deactivate()
-{
-	Module::Deactivate();
-
-	//app->playerModule->Deactivate();
-	app->map->Deactivate();
-	//app->physics->Deactivate();
-	//app->enemies->Deactivate();
-}
-void Scene::InitMapLevel()
-{
-	app->map->mapData.Clear();
-
-	app->map->Load("level_1.tmx");
-
-	MapData* mapData = &app->map->mapData;
-
-	// Physworld box
-	uint size = mapData->layers.count();
-
-	MapLayer* collisionLayer = NULL;
-
-	for (uint i = 0; i < size; i++) {
-		if (mapData->layers[i]->name == "Collisions") {
-			collisionLayer = mapData->layers[i];
-			break;
-		}
+	if (index >= 0)
+	{
+		gameObjects.del(gameObjects.At(index));
 	}
 }
--- Game/Source/Scene.h
+++ Game/Source/Scene.h
@@ -1,35 +1,24 @@
 #ifndef __SCENE_H__
 #define __SCENE_H__
 
-#include "Module.h"
-#include "PhysicBody.h"
-#include "PhysicWorld.h"
-
+#include "App.h"
+#include "Globals.h"
+#include "GameObject.h"
+#include "List.h"
 struct SDL_Texture;
 
+class PhysicWorld;
 class PhysicBody;
-class PhysWorld;
 
-#define BOUNCER_TIME 200
-
-struct Bouncer
-{
-	Bouncer() : texture(NULL), hit_timer(0), fx(0)
-	{}
-
-	SDL_Texture* texture;
-	Uint32 hit_timer;
-	uint fx;
-};
-
-enum lightTypes
-{
-	tiny,
-	medium,
-	big
-};
 class Scene : public Module
 {
+protected:
+
+	int ID;
+
+	App* _app;
+
+	List<SDL_Texture*> sceneTextures;
 public:
 
 	Scene();
@@ -43,6 +32,7 @@ public:
 	// Called before the first frame
 	bool Start();
 
+	void InitScene();
 	// Called before all Updates
 	bool PreUpdate();
 
@@ -55,26 +45,37 @@ public:
 	// Called before quitting
 	bool CleanUp();
 
-	// Called to activate module
-	void Activate();
+	void DestroyGameObject(GameObject* gameObject);
 
-	// called to deactivate module
-	void Deactivate();
+	int getID()
+	{
+		return ID;
+	}
 
-	void InitMapLevel();
-private:
-	SDL_Texture* img;
+	int lastID = 0;
 
-public:
-	SDL_Rect rect = { 10,10,500,500 };
-	SDL_Rect rect2 = { 10,10,20,20 };
+	int playerX;
+	int playerY;
 
+	SDL_Texture* bg[4];
+
+	SDL_Rect rect = { 10,10,10,10 };
+	SDL_Rect rect2 = { 10,10,10,10 };
+	SDL_Rect rect3 = { 10,10,10,10 };
 	PhysicBody* body;
 	PhysicBody* body2;
-	PhysicBody* body3;
-	PhysicBody* body4;
 
-	PhysWorld* world;
+	PhysicBody* walls[3];
+	PhysicBody* floor[6];
+
+	PhysicWorld* world;
+
+	GameObject* testGO = nullptr;
+	GameObject* testGO2 = nullptr;
+
+	List<GameObject*> gameObjects;
+private:
+	SDL_Texture* img;
 };
 
 #endif // __SCENE_H__
